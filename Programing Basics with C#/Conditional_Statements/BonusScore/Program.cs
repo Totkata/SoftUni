47eb613c3002bdf14bfb664d5d97326e3509b71c
Totkata <toti1471123@gmail.com>
2020-01-23 12:16:50 +0200

    Add files
    Conditional Statements

---
+++ Programing Basics with C#/Conditional_Statements/BonusScore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingPoints = int.Parse(Console.ReadLine());
+
+            double bonusPoints = 0;
+
+            if (startingPoints < 100)
+            {
+                bonusPoints += 5;
+            }
+            else if (startingPoints > 100 && startingPoints < 1000)
+            {
+                bonusPoints += startingPoints * 0.2;
+            }
+            else if (startingPoints > 1000)
+            {
+                bonusPoints += startingPoints * 0.1;
+            }
+
+            if (startingPoints % 2 == 0)
+            {
+                bonusPoints += 1;
+            }
+
+            if (startingPoints % 5 == 0 && startingPoints % 2 != 0)
+            {
+                bonusPoints += 2;
+            }
+
+            double totalPoints = bonusPoints + startingPoints;
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(totalPoints);
+        }
+    }
+}
---
+++ Programing Basics with C#/Conditional_Statements/MetricConverter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberToConvert = double.Parse(Console.ReadLine());
+
+            string type = Console.ReadLine();
+            string typeToConvert = Console.ReadLine();
+
+            double converted = 0;
+            if (type == "m")
+            {
+                if (typeToConvert == "mm")
+                {
+                    converted = numberToConvert * 1000;
+                    Console.WriteLine($"{converted:f3}");
+                }
+                else if (typeToConvert == "cm")
+                {
+                    converted = numberToConvert * 100;
+                    Console.WriteLine($"{converted:f3}");
+                }
+            }
+            else if (type == "mm")
+            {
+                if (typeToConvert == "m")
+                {
+                    converted = numberToConvert / 1000;
+                    Console.WriteLine($"{converted:f3}");
+                }
+                else if (typeToConvert == "cm")
+                {
+                    converted = numberToConvert / 10;
+                    Console.WriteLine($"{converted:f3}");
+                }
+            }
+            else if (type == "cm")
+            {
+                if (typeToConvert == "mm")
+                {
+                    converted = numberToConvert * 10;
+                    Console.WriteLine($"{converted:f3}");
+                }
+                else if (typeToConvert == "m")
+                {
+                    converted = numberToConvert / 100;
+                    Console.WriteLine($"{converted:f3}");
+                }
+            }
+        }
+    }
+}
