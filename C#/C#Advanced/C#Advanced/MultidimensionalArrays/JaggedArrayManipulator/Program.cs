846fa893937a35ac4a6c1d639c93e3a67e2d5a8b
Totkata <toti1471123@gmail.com>
2020-03-08 11:00:56 +0200

    Add files
    Multidimentional Arrays

---
+++ C#/C#Advanced/C#Advanced/MultidimensionalArrays/JaggedArrayManipulator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[][] matrix = new double[n][];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i] = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                bool check = false;
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (row < n - 1)
+                    {
+                        if (matrix[row].Length == matrix[row + 1].Length)
+                        {
+                            matrix[row][col] *= 2;
+                            matrix[row + 1][col] *= 2;
+                        }
+                        else
+                        {
+                            matrix[row][col] /= 2;
+                            check = true;
+                        }
+                    }
+                }
+                if (check)
+                {
+                    for (int i = 0; i < matrix[row + 1].Length; i++)
+                    {
+                        matrix[row + 1][i] = matrix[row + 1][i] / 2.0;
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] splitedCommand = command.Split(" ").ToArray();
+
+                string cmd = splitedCommand[0];
+
+                int row = int.Parse(splitedCommand[1]);
+                int col = int.Parse(splitedCommand[2]);
+                int value = int.Parse(splitedCommand[3]);
+
+                if (cmd == "Add" && row >= 0 && col >= 0) 
+                {
+                    if (matrix.GetLength(0) >= row && matrix[row].Length > col)
+                    {
+                        matrix[row][col] += value;
+                    }
+                }
+                else if (cmd == "Subtract" && row >= 0 && col >= 0)
+                {
+                    if (matrix.GetLength(0) >= row && matrix[row].Length > col)
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine(string.Join(" ", matrix[i]));
+            }
+        }
+    }
+}
---
+++ C#/C#Advanced/C#Advanced/MultidimensionalArrays/SnakeMoves/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int n = dimensions[0];
+            int m = dimensions[1];
+
+            string[,] matrix = new string[n,m];
+
+            string input = Console.ReadLine();
+
+            Queue<char> fill = new Queue<char>();
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                while (fill.Count < m)
+                {
+                    foreach (var item in input)
+                    {
+                        fill.Enqueue(item);
+                    }
+                }
+
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = fill.Peek().ToString();
+                        fill.Dequeue();
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = fill.Peek().ToString();
+                        fill.Dequeue();
+                    }
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
