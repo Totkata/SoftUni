b14a28878ce71c721e3aff08faacc9834ed28e10
Totkata <toti1471123@gmail.com>
2020-03-11 11:52:41 +0200

    Add files
    Multidimensional Arrays

---
+++ C#/C#Advanced/C#Advanced/MultidimensionalArrays/Bombs/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            string[] bombs = Console.ReadLine().Split(" ").ToArray();
+
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                int[] bombIndex = bombs[i].Split(",")
+                    .Select(int.Parse).ToArray();
+                int rowIndex = bombIndex[0];
+                int colIndex = bombIndex[1];
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.Length; col++)
+                    {
+                        if (row == rowIndex && col == colIndex)
+                        {
+                            int bomb = matrix[row][col];
+
+                            if (bomb > 0)
+                            {
+                                matrix[row][col] = 0;
+
+                                if (rowIndex + 1 < matrix.GetLength(0))
+                                {
+                                    if (matrix[row + 1][col] > 0)
+                                    {
+                                        matrix[row + 1][col] -= bomb;
+                                    }
+                                }
+                                if (rowIndex - 1 >= 0)
+                                {
+                                    if (matrix[row - 1][col] > 0)
+                                    {
+                                        matrix[row - 1][col] -= bomb;
+                                    }
+                                }
+                                if (colIndex + 1 < matrix.Length)
+                                {
+                                    if (matrix[row][col + 1] > 0)
+                                    {
+                                        matrix[row][col + 1] -= bomb;
+                                    }
+                                }
+                                if (colIndex - 1 >= 0)
+                                {
+                                    if (matrix[row][col - 1] > 0)
+                                    {
+                                        matrix[row][col - 1] -= bomb;
+                                    }
+                                }
+                                if (rowIndex - 1 >= 0 && colIndex - 1 >= 0)
+                                {
+                                    if (matrix[row - 1][col - 1] > 0)
+                                    {
+                                        matrix[row - 1][col - 1] -= bomb;
+                                    }
+                                }
+                                if (rowIndex + 1 < matrix.GetLength(0) && colIndex + 1 < matrix.Length)
+                                {
+                                    if (matrix[row + 1][col + 1] > 0)
+                                    {
+                                        matrix[row + 1][col + 1] -= bomb;
+                                    }
+                                }
+                                if (rowIndex + 1 < matrix.GetLength(0) && colIndex - 1 >= 0)
+                                {
+                                    if (matrix[row + 1][col - 1] > 0)
+                                    {
+                                        matrix[row + 1][col - 1] -= bomb;
+                                    }
+                                }
+                                if (colIndex + 1 < matrix.Length && rowIndex - 1 >= 0)
+                                {
+                                    if (matrix[row - 1][col + 1] > 0)
+                                    {
+                                        matrix[row - 1][col + 1] -= bomb;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int aliveCellsCount = 0;
+            int aliveCellsSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.Length; col++)
+                {
+                    if (matrix[row][col] > 0)
+                    {
+                        aliveCellsCount++;
+                        aliveCellsSum += matrix[row][col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCellsCount}");
+            Console.WriteLine($"Sum: {aliveCellsSum}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                Console.WriteLine(string.Join(" ",matrix[row]));
+            }
+        }
+    }
+}
---
+++ C#/C#Advanced/C#Advanced/MultidimensionalArrays/Miner/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+
+namespace Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] comands = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+
+            string[][] field = new string[n][];
+
+            int[] minerIndex = new int[2];
+            int totalCoalCount = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                field[row] = Console.ReadLine().Split(" ").ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    if (field[row][col] == "s")
+                    {
+                        minerIndex[0] = row;
+                        minerIndex[1] = col;
+                    }
+                    if (field[row][col] == "c")
+                    {
+                        totalCoalCount++;
+                    }
+                }
+            }
+
+            int coalCOunt = 0;
+
+            int r = minerIndex[0];
+            int c = minerIndex[1];
+
+            foreach (var comand in comands)
+            {
+                if (comand == "up")
+                {
+                    if (r - 1 >= 0)
+                    {
+                        field[r][c] = "*";
+                        r -= 1;
+
+                        if (field[r][c] == "c")
+                        {
+                            coalCOunt++;
+                            totalCoalCount--;
+                        }
+                        else if (field[r][c] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({r}, {c})");
+                            return;
+                        }
+                        field[r][c] = "s";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (comand == "right")
+                {
+                    if (c + 1 < n)
+                    {
+                        field[r][c] = "*";
+                        c += 1;
+
+                        if (field[r][c] == "c")
+                        {
+                            coalCOunt++;
+                            totalCoalCount--;
+                        }
+                        else if (field[r][c] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({r}, {c})");
+                            return;
+                        }
+                        field[r][c] = "s";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (comand == "down")
+                {
+                    if (r + 1 < n)
+                    {
+                        field[r][c] = "*";
+                        r += 1;
+
+                        if (field[r][c] == "c")
+                        {
+                            coalCOunt++;
+                            totalCoalCount--;
+                        }
+                        else if (field[r][c] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({r}, {c})");
+                            return;
+                        }
+                        field[r][c] = "s";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (comand == "left")
+                {
+                    if (c - 1 >= 0)
+                    {
+                        field[r][c] = "*";
+                        c -= 1;
+
+                        if (field[r][c] == "c")
+                        {
+                            coalCOunt++;
+                            totalCoalCount--;
+                        }
+                        else if (field[r][c] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({r}, {c})");
+                            return;
+                        }
+                        field[r][c] = "s";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                if (totalCoalCount == 0)
+                {
+                    Console.WriteLine($"You collected all coals! ({r}, {c})");
+                    return;
+                }
+            }
+            Console.WriteLine($"{totalCoalCount} coals left. ({r}, {c})");
+        }
+    }
+}
