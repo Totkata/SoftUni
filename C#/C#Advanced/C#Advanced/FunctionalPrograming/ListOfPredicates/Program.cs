c9fd736cd8c473ae13b20c65dadbc20901fd83aa
Totkata <toti1471123@gmail.com>
2020-04-01 18:13:50 +0300

    Add files
    Functional Programing

---
+++ C#/C#Advanced/C#Advanced/FunctionalPrograming/ListOfPredicates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            var deviders = Console.ReadLine()
+                .Split()
+                .Distinct()
+                .Select(int.Parse)
+                .ToList();
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            deviders.ForEach(d => predicates.Add(n => n % d == 0));
+            List<int> result = new List<int>();
+
+
+            for (int i = 1; i <= x; i++)
+            {
+                bool isDivisible = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(i))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    result.Add(i);
+                }
+            }
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
---
+++ C#/C#Advanced/C#Advanced/FunctionalPrograming/PredicateForNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var result = new List<string>();
+            arr.Where(n => n.Length <= lenght)
+                .ToList()
+                .ForEach(result.Add);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
