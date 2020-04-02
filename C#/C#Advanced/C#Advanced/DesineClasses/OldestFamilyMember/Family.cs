51ba048f7ab012a4573f6d1a047128419adbff6a
Totkata <toti1471123@gmail.com>
2020-04-02 21:07:33 +0300

    Add file
    Define classes

---
+++ C#/C#Advanced/C#Advanced/DesineClasses/OldestFamilyMember/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            familyMembers = new List<Person>();
+        }
+        public void AddFamilyMember(Person member)
+        {
+            familyMembers.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            Person oldest = familyMembers.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldest;
+        }
+
+    }
+}
---
+++ C#/C#Advanced/C#Advanced/DesineClasses/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ").ToArray();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person newMember = new Person(name,age);
+                family.AddFamilyMember(newMember);
+            }
+            Person oldest = family.GetOldestMember();
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
