c8cfe4920036ffaf04c7f24f1c30ada8e2c019e9
Totkata <toti1471123@gmail.com>
2020-04-03 09:36:37 +0300

    Add files
    Define Classes

---
+++ C#/C#Advanced/C#Advanced/DesineClasses/CreatingConstructors/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age)
+            : this()
+        {
+            Age = age;
+        }
+        public Person(string name,int age)
+            : this(age)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //public string Name
+        //{
+        //    get => this.name;
+        //    set => this.name = value;
+        //}
+        //public int Age
+        //{
+        //    get => this.age;
+        //    set => this.age = value;
+        //}
+    }
+}
---
+++ C#/C#Advanced/C#Advanced/DesineClasses/OpinionPoll/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var poll = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                if (age > 30)
+                {
+                    poll.Add(new Person(name, age));
+                }
+            }
+            foreach (var person in poll.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
